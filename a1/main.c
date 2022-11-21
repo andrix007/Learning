ac01d915da082477a92846da2fd3f69bce0dfd5c
Andrei Bancila <andreirbancila@gmail.com>
2022-11-21 23:16:14 +0100

    added precision

---
+++ a1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double rahat = 1234.123456789;
+    printf("%20.3lf\n",rahat);
+    //20 = space + cate cifre inainte de .
+    //3 = nr zecimale rotunjite
+    return 0;
+}
